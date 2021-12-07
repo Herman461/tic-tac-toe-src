07ad91f017ba2c285637a88959006a504960cf72
Herman461 <zhukovskiyherman@gmail.com>
2021-12-07 14:41:20 +0400

    v0.2

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,7 @@ cd dist
 # если вы публикуете на пользовательский домен
 # echo 'www.example.com' > CNAME
 
+git init
 git add -A
 git commit -m 'v0.2'
 git push -f git@github.com:Herman461/tic-tac-toe.git master:gh-pages
